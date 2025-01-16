bdbd316a4f9811b9d2e90bf451ff9c4b8e427509
kar99k <krthkeyanlee@gmail.com>
2025-01-16 23:26:50 +0530

    - Clerk auth sign-in and sign-up setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function SetupPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
